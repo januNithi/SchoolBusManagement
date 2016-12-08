e959e5e9b1755e71cbdf9e02c3f49cac1057e0e1
januNithi <csejananim@gmail.com>
2016-12-08 19:00:59 +0530

    Dashboard Have done with map and graph.

--- public/app/core/module.js
+++ public/app/core/module.js
@@ -1,1 +1,5 @@
-angular.module('myApp',['ngRoute']);
+angular.module('myApp',[
+    'ngRoute',
+    'googlechart',
+    'google-maps'
+]);
--- public/app/home/homeController.js
+++ public/app/home/homeController.js
@@ -18,13 +18,56 @@
 
     function homeController($scope,$window,homeService){
 
+        angular.extend($scope, {
+            centerProperty: {
+                lat: 11.015,
+                lng: 76.96
+            },
+            zoomProperty: 8,
+            markersProperty: [ {
+                latitude: 11.015,
+                longitude: 76.96
+            }],
+            clickedLatitudeProperty: null,
+            clickedLongitudeProperty: null,
+        });
+
         $scope.showSelectable = function (value) {
 
             if(value == 'home'){
                 return 'selected';
             }
 
-        }
+        };
+
+        $scope.myChartObject = {};
+
+        $scope.myChartObject.type = "PieChart";
+
+        $scope.onions = [
+            {v: "Bus On Time Start"},
+            {v: 18},
+        ];
+
+        $scope.myChartObject.data = {"cols": [
+            {id: "t", label: "Topping", type: "string"},
+            {id: "s", label: "Slices", type: "number"}
+        ], "rows": [
+            {c: [
+                {v: "Bus On Time Arrival"},
+                {v: 15},
+            ]},
+            {c: $scope.onions},
+            {c: [
+                {v: "Bus Delay"},
+                {v: 5}
+            ]}
+        ]};
+
+        $scope.myChartObject.options = {
+            'title': 'Bus Travel Reports'
+        };
+
     }
 })();
 
