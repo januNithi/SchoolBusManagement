97844cf01b083fc1351ebd68a6b85b06bfc3c277
januNithi <csejananim@gmail.com>
2016-12-09 16:22:14 +0530

    route changes in express.js

--- config/express.js
+++ config/express.js
@@ -19,8 +19,7 @@ module.exports=function(){
     app.use(express.static('./public'));
 
     require('../route/layout.server.route.js')(app);
-    require('../route/busRegistration.server.route.js')(app);
-    require('../route/gpsUnitRegistration.server.route.js')(app);
+    require('../route/register.server.route.js')(app);
     
     return app;
 };
