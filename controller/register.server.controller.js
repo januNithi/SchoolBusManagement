e87f772c53899ff3e6d47b313c2a5439aed06c5f
CCS DEPT <premvishnnu@gmail.com>
2016-12-13 12:47:44 +0530

    Merge branch 'master' of F:\Projects\SchoolBusManagements with conflicts.

--- controller/register.server.controller.js
+++ controller/register.server.controller.js
@@ -33,7 +33,7 @@ var Filestorage=multer.diskStorage({
         req.session.liscence=file.fieldname + '-' + file.originalname;
     }
 });
-var fileupload=multer({storage:Filestorage}).single('file1');
+var fileupload=multer({storage:Filestorage}).single('driverLiscence1');
 
 exports.busRegDetails=function (req,res) {
 
@@ -213,17 +213,3 @@ exports.postDriverDetail=function (req,res) {
 
 
 };
-
-function decodeBase64Image(dataString) {
-    var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
-        response = {};
-
-    if (matches.length !== 3) {
-        return new Error('Invalid input string');
-    }
-
-    response.type = matches[1];
-    response.data = new Buffer(matches[2], 'base64');
-
-    return response;
-}
