0790d37ff975ccb452d3259aad26d945571399ba
januNithi <csejananim@gmail.com>
2017-03-03 12:38:23 +0530

    Changes for Distance.

--- config/db/summaryReportManager.js
+++ config/db/summaryReportManager.js
@@ -11,7 +11,12 @@ var con = mysql.createPool(configObj.database);
 
 
 exports.getSummary = function (id,from,to,cb) {
-    var query = "SELECT d.unitName,d.id,d.uniqueid,p.deviceid,p.servertime,avg(p.speed) as avgspeed, max(p.speed) as maxspeed,(select attributes from positions where deviceid IN ("+id+") and  positions.devicetime between '"+from+"' and '"+to+"' ORDER BY id ASC limit 1) as attributes1,(select attributes from positions where deviceid IN ("+id+") and positions.devicetime between '"+from+"' and '"+to+"' ORDER BY id DESC limit 1) as attributes2,b.busCode,b.gpsUnit,b.id as busId from ";
+    var query = "SELECT d.unitName,d.id,d.uniqueid,p.deviceid,p.servertime,avg(p.speed) as avgspeed,";
+    query += " max(p.speed) as maxspeed,(select attributes from positions where deviceid = p.deviceid ";
+    query += " and  positions.devicetime between '"+from+"' and '"+to+"' ORDER BY id ASC limit 1) as";
+    query += " attributes1,(select attributes from positions where deviceid  = p.deviceid and positions.devicetime";
+    query += " between '"+from+"' and '"+to+"' ORDER BY id DESC limit 1) as attributes2,b.busCode,";
+    query += " b.gpsUnit,b.id as busId from ";
         query+="positions p left JOIN gpsunit d on d.id = p.deviceid left JOIN  bus b on b.gpsUnit=d.id WHERE p.deviceid IN ("+id+") and (p.devicetime between '"+from+"' and '"+to+"') group by  p.deviceid";
 
     console.log(query);
